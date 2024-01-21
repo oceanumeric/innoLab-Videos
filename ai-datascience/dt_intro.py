f4d02cd4e6dc6e2796f8eb965d1d5dd2e4d05819
oceanumeric <numerical.ocean@gmail.com>
2024-01-21 02:43:51 +0100

    [update]

--- ai-datascience/dt_intro.py
+++ ai-datascience/dt_intro.py
@@ -118,8 +118,10 @@ class Intro(Scene):
 
         # play arrow_table, arrow_json, and arrow_text_file animations
         self.play(
-            GrowArrow(arrow_table), GrowArrow(arrow_json), GrowArrow(arrow_text_file),
-            run_time=3
+            GrowArrow(arrow_table),
+            GrowArrow(arrow_json),
+            GrowArrow(arrow_text_file),
+            run_time=3,
         )
 
         # remove everything
@@ -129,7 +131,7 @@ class Intro(Scene):
             FadeOut(matrix),
             FadeOut(arrow_table),
             FadeOut(arrow_json),
-            FadeOut(arrow_text_file)
+            FadeOut(arrow_text_file),
         )
 
         # add image from './images/movie_matrix.jpg'
@@ -138,8 +140,9 @@ class Intro(Scene):
         image.scale(0.3)
         image.move_to(RIGHT * 3)
         # add to the screen
-        self.play(GrowFromCenter(image), FadeOut(table), FadeOut(json), FadeOut(text_file))
-
+        self.play(
+            GrowFromCenter(image), FadeOut(table), FadeOut(json), FadeOut(text_file)
+        )
 
         # add text 'With my 10 years of experience' to the left of image
         text_image = Tex("With my 10 years of experience")
@@ -155,7 +158,6 @@ class Intro(Scene):
         # play text_image and text_image2 animations
         self.play(FadeIn(text_image))
         self.play(FadeIn(text_image2))
-
 
         # remove everything
         self.play(FadeOut(image), FadeOut(text_image), FadeOut(text_image2))
@@ -175,8 +177,6 @@ class Intro(Scene):
         text_image3.scale(1.5)
         # play text_image3 animation
         self.play(FadeIn(text_image3))
-
-    
 
 
 # new chapter ch1
@@ -272,6 +272,295 @@ class Ch1(Scene):
         # play table_group animation
         self.play(MoveToTarget(table_group))
 
-        # add text 'data.table' on the top of the screen
- 
-    
+        # add text 'row' on left of brace_table2
+        text_row = Tex("row-i")
+        # set color '#FD8927' for text_row
+        text_row.set_color("#FD8927")
+        # make it vertical
+        text_row.rotate(PI / 2)
+        # move text_row to left of brace_table2
+        text_row.next_to(brace_table2, LEFT * 0.5)
+        # play text_row animation
+        self.play(FadeIn(text_row))
+
+        # add text 'column' on top of brace_table
+        text_column = Tex("column-j")
+        # set color '#E1181D' for text_column
+        text_column.set_color("#E1181D")
+        # move text_column to top of brace_table
+        text_column.next_to(brace_table, UP * 0.5)
+        # play text_column animation
+        self.play(FadeIn(text_column))
+
+        # remove brace_table, brace_table2, text_row, and text_column
+        self.play(
+            FadeOut(brace_table),
+            FadeOut(brace_table2),
+            FadeOut(text_row),
+            FadeOut(text_column),
+        )
+
+        # add text 'data[i, j, by]' on the top of the screen
+        text_data = Tex("data[i, j, by]")
+        # set color '#7f5af0' for text_data
+        text_data.set_color("#7f5af0")
+        # scale it up to 1.2
+        text_data.scale(1.5)
+        # move text_data to the top of the screen
+        text_data.move_to(UP * 3)
+
+        # scale table down to 0.7 and move it to the right of the screen
+        table.generate_target()
+        table.target.scale(0.7)
+        table.target.move_to(RIGHT * 3 + UP * 0.5)
+        # play table animation
+        self.play(FadeIn(text_data), MoveToTarget(table))
+
+        # add code block 'DT[i, j, by]' on the left of table
+        code_snippet1 = """data[i, j, by]"""
+        code_block = Code(
+            code=code_snippet1,
+            background="window",
+            language="R",
+            font="Monospace",
+            line_spacing=1,
+        )
+        # move code_block to the left of table
+        code_block.next_to(table, LEFT * 3)
+        # play code_block animation
+        self.play(FadeIn(code_block))
+
+        # remove code_block
+        self.play(FadeOut(code_block))
+        # add new code block 'data[1:2,]' on the left of table
+        code_snippet2 = """data[2,]"""
+        code_block2 = Code(
+            code=code_snippet2,
+            background="window",
+            language="R",
+            font="Monospace",
+            line_spacing=1,
+        )
+        # now highlight the third row of the table
+        row2_highlight = SurroundingRectangle(table.get_rows()[2])
+        # move code_block2 to the left of table
+        code_block2.next_to(table, LEFT * 5)
+        # play code_block2 animation
+        self.play(FadeIn(code_block2), FadeIn(row2_highlight))
+
+        # add text 'Using i position to select rows'
+        # and move down to 1.5
+        text_i = Tex("Using i position to select rows")
+        # no need to set color for text_i
+        text_i.scale(1.5)
+        text_i.shift(DOWN * 2)
+        # play text_i animation
+        self.play(Write(text_i))
+
+        # remove table, code_block2, and text_i
+        self.play(
+            FadeOut(table),
+            FadeOut(code_block2),
+            FadeOut(text_i),
+            FadeOut(row2_highlight),
+        )
+
+        # print out elements of table
+
+        # add table again
+        self.play(GrowFromCenter(table))
+
+        # add code block
+        code_snippet = '''
+        data[, 2:3]
+        data[, .(name, family_name)]'''
+
+        code_block = Code(
+            code=code_snippet,
+            background="window",
+            language="R",
+            font="Monospace",
+            line_spacing=1,
+        )
+
+        # scale down code_block to 0.7
+        code_block.scale(0.85)
+
+        # move code_block to the left of table
+        code_block.next_to(table, LEFT * 2)
+
+        # add highlight for columns 2 and 3
+        col2_highlight = SurroundingRectangle(table.get_columns()[1:3])
+        # play code_block animation
+        self.play(FadeIn(code_block), FadeIn(col2_highlight))
+
+
+        # add text 'Using j position to select columns'
+        # and move down to 1.5
+        text_j = Tex("Using j position to select columns")
+        # no need to set color for text_j
+        text_j.scale(1.5)
+        text_j.shift(DOWN * 2)
+        # play text_j animation
+        self.play(Write(text_j))
+
+        # remove table, code_block, and text_j
+        self.play(FadeOut(table), FadeOut(code_block), FadeOut(text_j), FadeOut(col2_highlight))
+
+        # create a new table
+        row1 = df.columns[[0, 1, 3, 6]].to_list()
+        row2 = df.iloc[0, [0, 1, 3, 6]].to_list()
+        # convert row2 to string
+        row2 = [str(i) for i in row2]
+
+        row3 = df.iloc[-2, [0, 1, 3, 6]].to_list()
+        # convert row3 to string
+        row3 = [str(i) for i in row3]
+
+        row4 = df.iloc[2, [0, 1, 3, 6]].to_list()
+        # convert row4 to string
+        row4 = [str(i) for i in row4]
+
+        row5 = df.iloc[3, [0, 1, 3, 6]].to_list()
+        # convert row5 to string
+        row5 = [str(i) for i in row5]
+
+        # add table from df
+        table = Table([row1, row2, row3, row4, row5])
+
+        # scale down table to 0.7
+        table.scale(0.7)
+
+        # move table to center of the screen
+        table.move_to(ORIGIN)
+
+        # add highlight for cell(1, 2) and cell(3, 2)
+        table.add_highlighted_cell((2, 3), color=GREEN)
+        table.add_highlighted_cell((4, 3), color=GREEN)
+
+        table[0].set_opacity(0)
+        table[1].set_opacity(0)
+
+
+        # add surrounding rectangle for row 2 and row 4
+        row2_highlight = SurroundingRectangle(table.get_rows()[1])
+        # move right
+        row2_highlight.shift(RIGHT * 3)
+        row3_highlight = SurroundingRectangle(table.get_rows()[3])
+        # move left
+        row3_highlight.shift(LEFT * 3)
+
+        # play row2_highlight and row3_highlight animations
+        self.play(FadeIn(table))
+
+        # let row2_highlight and row3_highlight
+        # fly in from the left side of the screen
+        self.play(
+            row2_highlight.animate.shift(LEFT * 3),
+            row3_highlight.animate.shift(RIGHT * 3),
+            run_time=2,
+        )
+
+        self.play(
+            table[0].animate.set_opacity(0.7),
+            table[1].animate.set_opacity(0.7),
+            run_time=2,
+        )
+
+        # add code block
+        code_snippet3 = '''
+        data %>%
+            .[category == 'top'] %>%
+            .[, .(age)] %>%
+            sum()'''
+        
+        code_block3 = Code(
+            code=code_snippet3,
+            background="window",
+            language="R",
+            font="Monospace",
+            line_spacing=1,
+        ).scale(0.8)
+
+
+
+        # remove row2_highlight and row3_highlight
+        # and move table to the right of the screen with scale 0.7
+        self.play(
+            FadeOut(row2_highlight),
+            FadeOut(row3_highlight),
+            table.animate.scale(0.7).move_to(RIGHT * 3),
+            run_time=2,
+        )
+
+        # move code_block3 to the left of table
+        code_block3.next_to(table, LEFT * 1.7)
+
+        # play code_block3 animation
+        self.play(Write(code_block3), run_time=8)
+
+        # remove everything
+        self.play(FadeOut(table), FadeOut(code_block3))
+
+
+        # create new table
+        # category	age_sum
+        # <chr>	<dbl>
+        # top	90
+        # middle	180
+        # bottom	380
+        table = Table(
+            [["category", "age_sum"],
+             ["top", "90"],
+             ["middle", "180"],
+             ["bottom", "380"]])
+        
+        # scale down table to 0.7
+        table.scale(0.7)
+
+        # move table to center of the screen
+        table.move_to(ORIGIN)
+
+        self.play(GrowFromCenter(table))
+
+
+        # move table to the right of the screen with scale 0.7
+        table.generate_target()
+        table.target.scale(0.7)
+        table.target.move_to(RIGHT * 3)
+        # play table animation
+        self.play(MoveToTarget(table))
+
+        # add code block
+        code_snippet4 = '''
+        data %>%
+            .[, .(age_sum = sum(age)), by = category]'''
+        
+        code_block4 = Code(
+            code=code_snippet4,
+            background="window",
+            language="R",
+            font="Monospace",
+            line_spacing=1,
+        ).scale(0.7)
+
+        # move code_block4 to the left of table
+        code_block4.next_to(table, LEFT * 1.7)
+
+        # play code_block4 animation
+        self.play(Write(code_block4), run_time=5)
+
+
+        # add text 'Using by to group rows'
+        # and move down to 1.5
+        text_by = Tex("Using by to group rows")
+        # no need to set color for text_by
+        text_by.scale(1.5)
+        text_by.shift(DOWN * 2)
+
+        # play text_by animation
+        self.play(Write(text_by))
+        
+
+        
+
